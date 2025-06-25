480420c03335f2df5c01b343d36fb28d2f286d32
bhuban <bhuban.kritim@gmail.com>
2025-06-25 14:44:33 +0545

    new

--- Backend/src/main/java/com/example/Backend/Repository/UserRepo.java
+++ Backend/src/main/java/com/example/Backend/Repository/UserRepo.java
@@ -1,6 +1,13 @@
 package com.example.Backend.Repository;
 
+import org.apache.catalina.User;
+
+import java.util.ArrayList;
+
 ;
 
 public class UserRepo {
+
+    private ArrayList<User> users;
+    Xcxcxcxcxcxcxcxcxcxcxcxcxccxcxcxcxc
 }
